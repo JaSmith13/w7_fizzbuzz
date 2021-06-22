72898eb79071c23d674ba02acf40b078ae7b5f1f
Alastair Smith <j.alastair.smith@outlook.com>
2021-06-22 11:00:51 +0100

    finishes mvp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react'
 import './App.css';
 
 function App() {
+
+  const [number, setNumber] = useState(1)
+  const [answer, setAnswer] = useState('')
+
+  const handleInc = () => {
+    setNumber(number + 1)
+  }
+
+  useEffect(() => {
+    console.log('useEffect triggered')
+    if (number % 3 === 0 && number % 5 === 0) {
+      setAnswer ("fizzbuzz")
+    } else if (number % 3 === 0) {
+      setAnswer ("fizz")
+    } else if (number % 5 === 0) {
+      setAnswer("buzz")
+    } else {
+      setAnswer(number);
+    }
+  }, [number])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>FizzBuzz</h1>
+      <button value={number} onClick={handleInc}>+</button>
+      <p>Number is {answer}</p>
+    </>
   );
 }
 
